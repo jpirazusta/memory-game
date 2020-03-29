fcb4a389d749459e9d175c0b36166cfd148b18eb
jpirazusta <juanpabloirazusta@gmail.com>
2020-03-28 21:10:11 -0300

    Adds: Start component. Working on Board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import React from 'react';
+
 import './App.css';
 import Start from './Start';
+import Board from './Board';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      started: false,
-      number: 0,
-      size: 0
+      step: 0,
+      finalNumber: "0",
+      numberOfPairs: "8"
     };
+    this.totalSteps = 2;
+  }
+
+  onConfigChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onIncrementStep = () => {
+    this.setState(previousState => ({
+      step: (previousState.step + 1) % this.totalSteps
+    }));
+  }
+
+  renderStep() {
+    const { finalNumber, numberOfPairs } = this.state;
+    switch (this.state.step) {
+      case 0:
+        return <Start
+          finalNumber={finalNumber}
+          numberOfPairs={numberOfPairs}
+          onConfigChange={this.onConfigChange}
+          onIncrementStep={this.onIncrementStep}
+        />;
+      case 1:
+        return <Board
+          finalNumber={finalNumber}
+          numberOfPairs={numberOfPairs}
+          onIncrementStep={this.onIncrementStep}
+        />;
+      default:
+        return null;
+    }
   }
 
   render() {
-    const { started, number, size } = this.state;
     return (
       <div className="main-container">
-        <Start display={started ? 'none' : 'block'} />
-        {/* <Board display={started} /> */}
+        { this.renderStep() }
       </div>
     );
   }
--- src/Start.js
+++ src/Start.js
@@ -1,19 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Start.css';
 import logo from './assets/white_logo.png';
 
-class Start extends React.Component {
-
-  render() {
-    return (
-      <div style={{ display: this.props.display }} className="start-container" >
-        <div className="logo-container">
-          <img className="logo" src={logo} alt="logo" />
-        </div>
-        Cantidad de pares de tarjetas (entre 4 y 32):
+function Start(props) {
+  console.log("Start props:", props);
+  return (
+    <div className="start-container" >
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
       </div>
-    );
-  }
+      <div>
+        <label>
+          Tamaño de tablero:
+            <div className="radio">
+            <label>
+              <input type="radio" value="8" name="numberOfPairs"
+                checked={props.numberOfPairs === '8'}
+                onChange={event => props.onConfigChange(event)} />
+                4x4
+              </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="18" name="numberOfPairs"
+                checked={props.numberOfPairs === '18'}
+                onChange={event => props.onConfigChange(event)} />
+                6x6
+              </label>
+          </div>
+        </label>
+      </div>
+      <div>
+        <label>
+          Número final (opcional):
+            <input type="number" value={props.finalNumber} name="finalNumber"
+            onChange={event => props.onConfigChange(event)} />
+        </label>
+      </div>
+      <button onClick={() => props.onIncrementStep()}>¡JUGAR!</button>
+    </div>
+  );
+}
+
+Start.propTypes = {
+  finalNumber: PropTypes.string,
+  numberOfPairs: PropTypes.string,
+  onConfigChange: PropTypes.func,
+  onIncrementStep: PropTypes.func
 }
 
 export default Start;
